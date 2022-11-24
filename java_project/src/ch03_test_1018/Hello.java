58f74856d2d2ffa14cca61ddd9089ac8af9dff5d
gowso22 <gowso89@naver.com>
2022-11-24 12:23:32 +0900

    저장일번

---
+++ java_project/src/ch03_test_1018/Hello.java
@@ -0,0 +1,13 @@
+package ch03_test_1018;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello 이상용 자바!");
+		/* 여러행 주석은 ctrl + shift + / */
+		// 포맷 정렬 ctrl + shift + f
+	}
+
+}
---
+++ java_project/src/test/Hello.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("hello 문자열 하나 입력하겠습니다.");
+
+	}
+
+}
